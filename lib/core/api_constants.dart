cd846c703d7f41e487ba5958e7200b39cd1037dd
Emy-04 <emy.ehab73@gmail.com>
2026-04-21 20:24:09 +0200

    Update api_constants.dart

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const String baseUrl = "https://api.themoviedb.org/3";
   static const String imageBaseUrl = "https://image.tmdb.org/t/p/w500";
-  static const String apiKey = "3911eb7ab1bca542a7c4c2d5ddc8c981";
-  static const String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTExZWI3YWIxYmNhNTQyYTdjNGMyZDVkZGM4Yzk4MSIsIm5iZiI6MTc2Mjk3MzM4NS41NjQsInN1YiI6IjY5MTRkNmM5OWY1M2JlMmUwMDRjOTJhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7bfSeWLYew6glevgfq2ZJS35G46uQuw8Q3kztiA4UAg";
+  static const String apiKey = "Add_Your_own";
+  static const String accessToken = "Add_your_own";
 }
